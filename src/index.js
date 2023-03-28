6408d01f79560f8c9306f3baf0d3d98d972524d6
yaya <ysanchez.1187@gmail.com>
2023-03-28 10:46:18 -0700

    added event listeners

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App';
-import AppClass from './components/AppClass';
+// import AppClass from './components/AppClass';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <AppClass />
+    <App />
   </React.StrictMode>
 );
 
