e836d738f13ac455ce375c03f47c5e43ef0613b1
yaya <ysanchez.1187@gmail.com>
2023-03-29 12:51:04 -0700

    extracting components

---
+++ src/components/TodoForm.jsx
@@ -0,0 +1,44 @@
+import React, {useState} from 'react'
+import PropTypes from 'prop-types';
+
+TodoForm.propTypes = {
+    addTodo: PropTypes.func.isRequired,
+    
+    }
+
+function TodoForm(props) {
+    const [todoInput, setTodoInput] = useState('');
+
+    function handleInput(event){
+        setTodoInput(event.target.value);
+      }
+
+      function handleSubmit(event){
+        event.preventDefault();
+
+        if(todoInput.trim().length === 0) {
+          return;
+        }
+
+// Se llamno a una funcion
+        props.addTodo(todoInput);
+
+        setTodoInput('');
+      }
+    
+  return (
+    <form action="#" onSubmit={handleSubmit}>
+    <input
+      type="text"
+      value={todoInput}
+      onChange={handleInput}
+      className="todo-input"
+      placeholder="What do you need to do?"
+    />
+  </form>
+  );
+}
+
+
+
+export default TodoForm
